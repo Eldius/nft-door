fb350010e42833ae4928c6e322cfbc21860c5cb7
eldius <eldiosantos@gmail.com>
2024-11-28 22:18:06 -0300

    Some refac...

--- internal/client/ethereum/client.go
+++ internal/client/ethereum/client.go
@@ -6,18 +6,53 @@ import (
 	"github.com/eldius/nft-pocs/internal/contracts/doorcontrol"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"time"
 )
+
+func getClient(endpoint string) (*ethclient.Client, error) {
+	fmt.Println("connecting to:", endpoint)
+	cl, err := ethclient.Dial(endpoint)
+	if err != nil {
+		err = fmt.Errorf("failed to connect eth client: %v", err)
+		return nil, err
+	}
+
+	return cl, nil
+}
+
+func authenticate(ctx context.Context, cl *ethclient.Client, pk string) (*bind.TransactOpts, error) {
+	chainID, err := cl.ChainID(ctx)
+	if err != nil {
+		err = fmt.Errorf("failed to connect to eth client: %v", err)
+		return nil, err
+	}
+
+	ecdsaPrivateKey, err := crypto.HexToECDSA(pk)
+	if err != nil {
+		err = fmt.Errorf("failed to connect to eth client: %v", err)
+		return nil, err
+	}
+	auth, err := bind.NewKeyedTransactorWithChainID(ecdsaPrivateKey, chainID)
+	if err != nil {
+		err = fmt.Errorf("failed to connect to eth client: %v", err)
+		return nil, err
+	}
+
+	return auth, nil
+}
+
+func deployContract(ctx context.Context, auth *bind.TransactOpts, cl *ethclient.Client) (common.Address, *types.Transaction, *doorcontrol.DoorControl, error) {
+	return doorcontrol.DeployDoorControl(auth, cl, auth.From)
+}
 
 func DeployContract(ctx context.Context, endpoint, pk, link string) error {
 	return nil
 }
 
 func Mint(ctx context.Context, endpoint, pk, link string) error {
-	fmt.Println("connecting to:", endpoint)
-	cl, err := ethclient.Dial(endpoint)
+	cl, err := getClient(endpoint)
 	if err != nil {
 		err = fmt.Errorf("failed to connect to eth client: %v", err)
 		return err
@@ -25,87 +60,59 @@ func Mint(ctx context.Context, endpoint, pk, link string) error {
 
 	defer cl.Close()
 
-	chainID, err := cl.ChainID(ctx)
+	auth, err := authenticate(ctx, cl, pk)
 	if err != nil {
-		err = fmt.Errorf("failed to connect to eth client: %v", err)
+		err = fmt.Errorf("failed to authenticate: %v", err)
 		return err
 	}
 
-	ecdsaPrivateKey, err := crypto.HexToECDSA(pk)
-	if err != nil {
-		err = fmt.Errorf("failed to connect to eth client: %v", err)
-		return err
-	}
-	auth, err := bind.NewKeyedTransactorWithChainID(ecdsaPrivateKey, chainID)
-	if err != nil {
-		err = fmt.Errorf("failed to connect to eth client: %v", err)
-		return err
-	}
+	//address, tx, instance, err := doorcontrol.DeployDoorControl(auth, cl, common.MaxAddress)
+	//if err != nil {
+	//	err = fmt.Errorf("failed to connect to eth client: %v", err)
+	//	return err
+	//}
+	//
+	//fmt.Printf("Contract pending deploy: 0x%x\n", address)
+	//fmt.Printf("Transaction waiting to be mined: 0x%x\n\n", tx.Hash())
+	//fmt.Printf("Contract pending deploy: 0x%x\n\n", tx.Hash())
+	//fmt.Printf("instance: 0x%+v\n\n", instance)
 
-	address, tx, instance, err := doorcontrol.DeployDoorControl(auth, cl, common.MaxAddress)
-	if err != nil {
-		err = fmt.Errorf("failed to connect to eth client: %v", err)
-		return err
-	}
+	address, tx, instance, err := deployContract(ctx, auth, cl)
 
-	fmt.Printf("Contract pending deploy: 0x%x\n", address)
-	fmt.Printf("Transaction waiting to be mined: 0x%x\n\n", tx.Hash())
-
-	fmt.Printf("Tx to address: %+v\n\n", tx.To())
-	fmt.Printf("Tx: %+v\n\n", tx)
-	fmt.Printf("Tx access list: %+v\n\n", tx.AccessList())
-	fmt.Printf("Address: %s\n\n", address.String())
-	time.Sleep(250 * time.Millisecond) // Allow it to be processed by the local node :P
-
-	owner, err := instance.Owner(&bind.CallOpts{
-		Pending: true,
-		Context: ctx,
-	})
-	if err != nil {
-		err = fmt.Errorf("failed to connect to eth client: %v", err)
-		return err
-	}
-
-	fmt.Printf("Owner: %s\n", owner.Hex())
-
-	// function call on `instance`. Retrieves pending name
-	name, err := instance.Name(&bind.CallOpts{Pending: true})
-	if err != nil {
-		err = fmt.Errorf("failed to connect to eth client: %v", err)
-		return err
-	}
-	fmt.Println("Pending name:", name)
-
-	dc, err := doorcontrol.NewDoorControl(address, cl)
-	if err != nil {
-		err = fmt.Errorf("failed to connect to eth client: %v", err)
-		return err
-	}
-
-	fmt.Printf("Contract pending deploy: 0x%x\n\n", tx.Hash())
-	fmt.Printf("dc: %+v\n", dc)
+	fmt.Printf("tx: %+v\n\n", tx)
+	fmt.Printf("instance: %+v\n\n", instance)
+	fmt.Printf("address: %+v\n\n", address)
 
 	session := &doorcontrol.DoorControlSession{
-		Contract: dc,
+		Contract: instance,
 		CallOpts: bind.CallOpts{
 			Pending: true,
 			Context: ctx,
 		},
-		TransactOpts: bind.TransactOpts{
-			From:     auth.From,
-			Signer:   auth.Signer,
-			GasLimit: uint64(3141592),
-		},
+		TransactOpts: *auth,
 	}
 
 	// Call the previous methods without the option parameters
-	tx1, err := session.SafeMint(address, link)
+	tx1, err := session.SafeMint(auth.From, link)
+	if err != nil {
+		err = fmt.Errorf("failed to mint nft: %v", err)
+		return err
+	}
+
+	fmt.Printf("tx1.Hash(): 0x%x\n", tx1.Hash())
+	fmt.Printf("tx1.Value(): 0x%x\n", tx1.Value())
+
+	return nil
+}
+
+func mint(instance *doorcontrol.DoorControlSession, auth *bind.TransactOpts, link string) error {
+	minted, err := instance.SafeMint(auth.From, link)
 	if err != nil {
 		err = fmt.Errorf("failed to connect to eth client: %v", err)
 		return err
 	}
 
-	fmt.Println("SafeMint tx1:", tx1)
+	fmt.Printf("Contract minted: 0x%x\n\n", minted)
 
 	return nil
 }
